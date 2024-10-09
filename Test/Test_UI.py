ea8ae3511fb435c193168144658cb3136ccceb7f
KseniaSok <xxeshas@gmail.com>
2024-10-09 22:19:37 +0300

    DIPLOMA

--- Test/Test_UI.py
+++ Test/Test_UI.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from Page.main import base_url_ui
+from main import base_url_ui
 
 
 @pytest.fixture()
---
+++ Test/main.py
@@ -0,0 +1,8 @@
+base_url = "https://api.kinopoisk.dev/v1.4/"
+api_key = "BWGASBS-XHMM6XA-PFKJ9Y3-M1WGGEG"
+
+headers = {
+    "X-API-KEY": api_key
+}
+
+base_url_ui = "https://www.kinopoisk.ru/?utm_referrer=www.google.com"
